aea2af0574877c8985189b6f80bfe66a07fc818b
mohitgoyat525 <montyits71@gmail.com>
2025-01-21 09:30:09 +0530

    add-changes

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { SearchIcon } from "../utils/icons";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="w-full max-w-[1128px] header-shadow rounded-[20px] flex items-center justify-between py-[10px] max-md:flex-wrap max-lg:gap-6 max-sm:gap-2 mt-[10px]">
-      <a href="/">
+      <Link to="/">
         <img
           src="/assets/images/svg/header-logo.svg"
           alt="logo"
           className="ps-[38px] max-md:ps-6"
         />
-      </a>
+      </Link>
       <div className=" max-md:hidden flex items-center rounded-full w-full max-w-[650px] justify-between border border-solid border-offBlue ps-[30px] pe-2 max-lg:ps-2">
         <input
           type="text"
@@ -22,10 +22,10 @@ const Header = () => {
         </p>
       </div>
       <div className="flex items-center gap-5">
-        <button className="text-darkBlack text-sm leading-6 font-normal">
+        <button className="text-darkBlack text-sm leading-6 font-normal transition-all ease-linear duration-300 hover:scale-110">
           Login
         </button>
-        <button className="me-3 bg-darkBlack text-white font-medium text-sm leading-6 rounded-[9px] w-full min-w-[105px] max-sm:min-w-[85px] py-[9px] h-[43px]">
+        <button className="me-3 bg-darkBlack border border-solid border-transparent hover:bg-white hover:text-darkBlack transition-all ease-linear duration-300 hover:border-darkBlack text-white font-medium text-sm leading-6 rounded-[9px] w-full min-w-[105px] max-sm:min-w-[85px] py-[9px] h-[43px]">
           Sign Up
         </button>
       </div>
